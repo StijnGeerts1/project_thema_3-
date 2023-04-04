4f90f0b5d3bc8c04037860c143692c128a4e38a6
StijnGeerts1 <stijngeerts1@edu-kw1c.nl>
2023-04-04 10:26:27 +0200

    project

--- Index.php
+++ Index.php
@@ -8,12 +8,29 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>sence</title>
     <link href="Styles/style.css" rel="stylesheet">
+    <link href="Styles/nav-foot.css" rel="stylesheet">
 </head>
 <body>
     <header class="hoofdpagina">
+        <img src="Images/sence_logo.png">
         <h1>"Eten moet spannend zijn zonder afbreuk te doen aan de authentieke smaak van de ingrediënten."</h1>
         <p>Dennis Middeldorp, Patron Cuisinier</p>
+        <a href="#">Werken bij Sence?</a>
         <div class="opvulling"></div>
     </header>
+    <main>
+        <div>
+            <div class="tekst">
+                <img src="../project_thema_3-/Images/OIP.jpg" class="dennis">
+                <h2>Dennis Middeldorp</h2>
+                <p>Patron Cuisinier Dennis Middeldorp kookt puur, creatief en verfijnd met alleen de allerbeste producten. De geboren en getogen Bosschenaar die in 2008 zijn geliefde Sense Restaurant oprichtte, haalt het beste uit dagverse en duurzame ingrediënten: "Bij het bedenken van een gerecht wil ik de essentie van smaken samenbrengen. Genieten van smaak, kleur, mondgevoel, de sfeer in het restaurant; alles moet kloppen."</p>
+                <p>Samen met zijn rechterhanden Chefs de Cuisine Peter Linnenbank, Meesterkok Johan Van Groeninge én zijn gepassioneerde en gedreven team, dat zich elke dag met evenveel bevlogenheid en teamgeest inzet, brengt hij de meest mooie smaken met een aziatische, moderne invloed samen. Zijn liefde voor de botanische gastronomie gaat hand in hand met zijn liefde voor vlees en vis. <br><br> Trots is hij op de samenwerking met lokale leveranciers en zijn vriendschap met het Hartenboeren Voedselbos waar hij en zijn team zelf de kruiden en groentjes plukken.</p>
+                <p>Sense is door Michelin in 2022 wederom en voor de elfde maal bekroond met een ster. Dennis ziet dat als een teken van waardering, maar zelfs duizend sterren veranderen niets aan zijn ambities: liefdevol koken en aandacht schenken aan zijn gasten vanuit een duurzame keuken met respect voor de natuur en haar seizoenen.</p>
+                <h2>Smaak en beleving</h2>
+                <p>Sense is loungy en modern en door Michelin in 2022 wederom voor de elfde keer bekroond met een Michelinster(*). Van het kleurrijke moderne interieur tot de exclusieve kaaswagen en de uitgebalanceerde wijnkaart van Maître-Sommelier Sven Heijkoop waarop zowel biologische ontdekkingen als klassieke grote wijnhuizen vertegenwoordigd zijn. Het restaurant in Den Bosch is ruim opgezet;</p>
+                <a class="button" href="#">lees meer over het menu</a>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
